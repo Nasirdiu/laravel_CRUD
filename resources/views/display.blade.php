0ed95292340003c184aebd20595e08018feb0b37
Md.Nasir Uddin <nasir.joypurhat@gmail.com>
2023-08-12 20:24:28 +0600

    Complate

--- resources/views/display.blade.php
+++ resources/views/display.blade.php
@@ -3,6 +3,12 @@
     <div class="mycontent">
         <div class="myapp">
             <h2 class="text-center">All Our Record <a href="{{route('home')}}" class="btn btn-sm btn-primary">Insert Your</a></h2>
+            @if (session()->has('message'))
+                <div class="alert alert-success">
+                    {{session()->get('message')}}
+                </div>
+
+            @endif
             <ul>
                 @foreach($students as $student)
                 <li>
@@ -11,6 +17,14 @@
                     Email-{{$student->email}}
                     <br>
                     Reg Number-{{$student->reg}}
+                    <br>
+                    <a href="{{route('edit.info',$student->id)}}" class="btn btn-sm btn-danger">Edit</a>
+                    <form action="{{route('delete.info')}}" method="POST" class="d-inline">
+                        @csrf
+                        <input type="hidden" value="{{$student->id}}" name="id">
+                        <input type="submit" value="delete" class="btn btn-sm btn-warning">
+
+                    </form>
                 </li>
                 @endforeach
             </ul>
--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="mycontent">
         <div class="myapp">
-            <h2 class="text-center">Insert Your Record <a href="{{route('display.info')}}" class="btn btn-sm btn-primary">View All Record</a></h2>
+            <h2 class="text-center">Edit Your Record <a href="{{route('display.info')}}" class="btn btn-sm btn-primary">View All Record</a></h2>
             @if ($errors->any())
                 <div class="alert alert-danger">
                     <ul>
@@ -12,19 +12,20 @@
                     </ul>
                 </div>
             @endif
-            <form action="{{route('store.info')}}" method="POST">
+            <form action="{{route('update.info')}}" method="POST">
                 @csrf
+                <input type="hidden" value="{{$student->id}}" name="id">
                 <div class="form-group mb-2">
                     <label for="" class="form-label">Name</label>
-                    <input type="text" name="name" class="form-control">
+                    <input type="text" name="name" class="form-control" value="{{$student->name}}">
                 </div>
                 <div class="form-group mb-2">
                     <label for="" class="form-label">Email</label>
-                    <input type="email" name="email" class="form-control">
+                    <input type="email" name="email" class="form-control" value="{{$student->email}}">
                 </div>
                 <div class="form-group mb-2">
                     <label for="" class="form-label">Reg Number</label>
-                    <input type="number" name="reg" class="form-control">
+                    <input type="number" name="reg" class="form-control" value="{{$student->reg}}">
                 </div>
                 <input type="submit" value="Submit Your Information" class="btn btn-primary w-100" >
             </form>
