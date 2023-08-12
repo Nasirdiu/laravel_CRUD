45f2fb6866a2ad49c132ec2059342b01bed61ae8
Md.Nasir Uddin <nasir.joypurhat@gmail.com>
2023-08-12 11:35:21 +0600

    home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,23 @@
-<h2>Home</h2>
+@extends('layouts')
+@section('content')
+    <div class="mycontent">
+        <div class="myapp">
+            <h2 class="text-center">Insert Your Record <a href="" class="btn btn-sm btn-primary">View All Record</a></h2>
+            <form action="">
+                <div class="form-group mb-2">
+                    <label for="" class="form-label">Name</label>
+                    <input type="text" name="name" class="form-control">
+                </div>
+                <div class="form-group mb-2">
+                    <label for="" class="form-label">Email</label>
+                    <input type="email" name="email" class="form-control">
+                </div>
+                <div class="form-group mb-2">
+                    <label for="" class="form-label">Reg Number</label>
+                    <input type="number" name="reg" class="form-control">
+                </div>
+                <input type="submit" value="Submit Your Information" class="btn btn-primary w-100" >
+            </form>
+        </div>
+    </div>
+@endsection
